c7faff9087b67b2ebee9119facbdbdfb69c9af0b
Oğuz Yıldız <oguz.yildiz@yenibiris.com>
2023-05-07 20:12:10 +0300

    App version 1.0.0

---
+++ Todo List/Models/ItemModel.swift
@@ -0,0 +1,25 @@
+//
+//  ItemModel.swift
+//  TodoList
+//
+//  Created by Oğuz YILDIZ on 27.04.2023.
+//
+
+import Foundation
+
+// Immutable Struct
+struct ItemModel: Identifiable, Codable {
+    let id: String
+    let title: String
+    let isCompleted: Bool
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func updateCompletion() -> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
+ }
---
+++ Todo List/Views/AddView.swift
@@ -0,0 +1,71 @@
+//
+//  AddView.swift
+//  Financier
+//
+//  Created by Oğuz YILDIZ on 27.04.2023.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
+    @State var textFieldText: String = ""
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height:55)
+                    .background(.gray.opacity(0.3))
+                    .cornerRadius(10)
+                Button(action: saveButtonPressed, label: {
+                    Text("Save".uppercased())
+                        .foregroundColor(.white)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                })
+            }
+            .padding(14)
+        }
+        .navigationTitle("Add an Item 🖋️")
+        .alert(isPresented: $showAlert, content: getAlert)
+    }
+    
+    
+    func saveButtonPressed(){
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        } else {
+            
+        }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item must be at least 3 characters! 😨"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AddView()
+        }
+        .environmentObject(ListViewModel())
+     }
+}
